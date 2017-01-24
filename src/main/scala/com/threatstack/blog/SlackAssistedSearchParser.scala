f33eed471f2295f9a06a479a9fe9db5291aa7433
Ryan Plessner <plessner.ryan@gmail.com>
2017-01-24 11:35:58 -0500

    Add Slack Search Example

---
+++ src/main/scala/com/threatstack/blog/SlackAssistedSearchParser.scala
@@ -0,0 +1,63 @@
+package com.threatstack.blog
+
+import org.joda.time.DateTime
+import org.parboiled2._
+
+sealed trait SlackSearchAst
+object SlackSearchAst {
+  case class SearchTerm(term: String) extends SlackSearchAst
+  case class SearchInChannel(search: SearchTerm, channel: String) extends SlackSearchAst
+  case class SearchMessagesFrom(searchTerm: SearchTerm, user: String) extends SlackSearchAst
+  case class SearchOnDate(searchTerm: SearchTerm, dateTime: DateTime) extends SlackSearchAst
+}
+
+/** An example of using Parboiled to parse the Slack Search Expression */
+class SlackAssistedSearchParser(val input: ParserInput) extends Parser {
+  import SlackSearchAst._
+
+  def InputLine = rule { SearchFilter ~ EOI }
+
+  def SearchFilter: Rule1[SlackSearchAst] = rule {
+    Search ~ DateFilter ~> SearchOnDate       |
+    Search ~ InChannel  ~> SearchInChannel    |
+    Search ~ FromFilter ~> SearchMessagesFrom |
+    Search
+  }
+
+  def DateFilter: Rule1[DateTime] = rule {
+    "on" ~ ':' ~ runSubParser(new DateParser(_).InputLine)
+  }
+
+  def Search = rule(Term ~> SearchTerm)
+
+  def InChannel: Rule1[String] = rule("in" ~ ":" ~ Term)
+
+  def FromFilter: Rule1[String] = rule("from" ~ ":" ~ Term)
+
+  def Term: Rule1[String] = rule { capture(oneOrMore(CharPredicate.AlphaNum)) ~ WhiteSpace }
+
+  def WhiteSpace = rule(zeroOrMore(WhiteSpaceChar))
+  def WhiteSpaceChar = CharPredicate(" ")
+}
+
+class DateParser(val input: ParserInput) extends Parser {
+  import CharPredicate._
+
+  def InputLine = rule(Date ~ EOI)
+
+  def Date: Rule1[DateTime] = rule {
+    Month ~ "/" ~ Day ~ "/" ~ Year ~> dateTime _ |
+    Year ~ "/" ~ Month ~ "/" ~ Day ~> ((year: Int, month: Int, day: Int) => dateTime(month, day, year))
+  }
+
+  def Day: Rule1[Int] = Digit2
+  def Month: Rule1[Int] = Digit2
+  def Year: Rule1[Int] = Digit4
+
+  def Digit2: Rule1[Int] = rule(capture(2.times(Digit)) ~> toInt _)
+  def Digit4: Rule1[Int] = rule(capture(4.times(Digit)) ~> toInt _)
+
+  private def toInt(string: String): Int = string.toInt
+  private def dateTime(month: Int, day: Int, year: Int): DateTime =
+    new DateTime(year, month, day, 0, 0, 0)
+}
---
+++ src/test/scala/com/threatstack/blog/ParboiledSlackSpec.scala
@@ -0,0 +1,91 @@
+package com.threatstack.blog
+
+import com.threatstack.blog.SlackSearchAst.{SearchInChannel, SearchOnDate, SearchMessagesFrom, SearchTerm}
+import org.joda.time.DateTime
+import org.parboiled2.ParseError
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.util.{Failure, Success}
+
+class ParboiledSlackSpec extends FlatSpec with Matchers {
+
+  "The Slack Search Filter" should "parse a term" in {
+    val term = "hello"
+    val parsed = new SlackAssistedSearchParser(term).InputLine.run().get
+    parsed shouldEqual SearchTerm("hello")
+  }
+
+  it should "not parse an empty input" in {
+    val term = ""
+    val parsed = new SlackAssistedSearchParser(term).InputLine.run()
+    assert(parsed.isFailure)
+  }
+
+  it should "parse a Channel Filter" in {
+    val term = "detective in:bigthings"
+    val parsed = new SlackAssistedSearchParser(term).InputLine.run().get
+    parsed shouldEqual SearchInChannel(SearchTerm("detective"), "bigthings")
+  }
+
+  it should "not parse a Channel Filter without a Search Term" in {
+    val term = "in:bigthings"
+    val parsed = new SlackAssistedSearchParser(term).InputLine.run()
+    assert(parsed.isFailure)
+  }
+
+  it should "parse a From Filter" in {
+    val term = "dangerzone from:thedetective"
+    val parsed = new SlackAssistedSearchParser(term).InputLine.run().get
+    parsed shouldEqual SearchMessagesFrom(SearchTerm("dangerzone"), "thedetective")
+  }
+
+  it should "not parse a From Filter without a Search Term" in {
+    val term = "from:thedetective"
+    val parsed = new SlackAssistedSearchParser(term).InputLine.run()
+    assert(parsed.isFailure)
+  }
+
+  it should "parse a US Date Filter" in {
+    val term = "hello on:12/10/2016"
+    val expectedTime = new DateTime(2016, 12, 10, 0, 0, 0)
+    val parser = new SlackAssistedSearchParser(term)
+    parser.InputLine.run() match {
+      case Failure(error: ParseError) => fail(parser.formatError(error))
+      case Failure(error) => fail(error)
+      case Success(parsed) =>
+        parsed shouldEqual SearchOnDate(SearchTerm("hello"), expectedTime)
+    }
+  }
+
+  it should "parse an Internaional Date Filter" in {
+    val term = "hello on:2016/12/10"
+    val expectedTime = new DateTime(2016, 12, 10, 0, 0, 0)
+    val parser = new SlackAssistedSearchParser(term)
+    parser.InputLine.run() match {
+      case Failure(error: ParseError) => fail(parser.formatError(error))
+      case Failure(error) => fail(error)
+      case Success(parsed) =>
+        parsed shouldEqual SearchOnDate(SearchTerm("hello"), expectedTime)
+    }
+  }
+
+  it should "not parse a US Date Filter without a Search Term" in {
+    val term = "on:10/12/2016"
+    val parsed = new SlackAssistedSearchParser(term).InputLine.run()
+    assert(parsed.isFailure)
+  }
+
+  "Dates" should "be parsed" in {
+    val date = "12/10/2016"
+    val parsed = new DateParser(date).InputLine.run()
+    assert(parsed.isSuccess)
+    parsed.get shouldEqual new DateTime(2016, 12, 10, 0, 0, 0)
+  }
+
+  it should "also work internationally" in {
+    val date = "2016/12/10"
+    val parsed = new DateParser(date).InputLine.run()
+    assert(parsed.isSuccess)
+    parsed.get shouldEqual new DateTime(2016, 12, 10, 0, 0, 0)
+  }
+}
